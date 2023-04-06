f49830a02315b923678c9114b4219642e542fc09
Andrew Willey <andrew.willey@five9.com>
2023-04-06 17:29:00 -0500

    working SF CAV Update

--- Salesforce/beforeDispo_controller.js
+++ Salesforce/beforeDispo_controller.js
@@ -4,65 +4,128 @@
     const hookApi = window.Five9.CrmSdk.hookApi();
     const interactionApi = window.Five9.CrmSdk.interactionApi();
 
-    function logMessage(message, prefix = "*******************") {
+    let selectedRecordId = "";
+
+    function logMessage(message, prefix = "[*****]", mode = "debug") {
       var d = new Date();
       var n = d.toLocaleString();
-      console.log(`${n} ${prefix} ${message}`);
+      if (mode === "debug") {
+        console.debug(`${n} ${prefix} ${message}`);
+      } else if (mode === "error") {
+        console.error(`${n} ${prefix} ${message}`);
+      } else {
+        console.log(`${n} ${prefix} ${message}`);
+      }
     }
 
     let cavsDict = {};
 
+    function updateSalesforceIdCAV(recordId) {
+      // IMPORTANT - The CAV MUST be on the campaign profile layout
+      let updateCavList = [
+        {
+          id: `${cavsDict["Salesforce"]["salesforce_id"]["id"]}`,
+          value: recordId,
+        },
+      ];
+      logMessage(`CAV update with cavList: ${JSON.stringify(updateCavList)}`);
+      interactionApi.setCav({
+        interactionId: selectedObject.interactionData.interactionId,
+        cavList: updateCavList,
+      });
+    }
+
     interactionApi.subscribe({
-      // on call accepted, get the domain CAVs
-      callAccepted: (callData) => {
-        logMessage(`Call Accepted ${JSON.stringify(callData)}`);
-      },
-      callStarted: params => {
-        interactionApi.getCav({ interactionId: params.callData.interactionId })
-          .then(domainCavs => {
-            // build a dictionary of CAVs by group and name for easy lookup
+      // on call accepted, get the domain CAVs and map to a dictionary for easy lookup
+      callStarted: (startedCall) => {
+        interactionApi
+          .getCav({ interactionId: startedCall.callData.interactionId })
+          .then((domainCavs) => {
             domainCavs.forEach((cav) => {
               cavsDict[cav.group] = cavsDict[cav.group] || {};
-              cavsDict[cav.group][cav.name] = cavsDict[cav.group][cav.name] || {};
+              cavsDict[cav.group][cav.name] =
+                cavsDict[cav.group][cav.name] || {};
               cavsDict[cav.group][cav.name]["id"] = cav.id;
               cavsDict[cav.group][cav.name]["type"] = cav.type;
               cavsDict[cav.group][cav.name]["restrictions"] = cav.restrictions;
             });
-            logMessage(`Interaction API got cavList: ${JSON.stringify(cavsDict)}`);
+            logMessage(
+              `Interaction API got cavList: ${JSON.stringify(cavsDict)}`
+            );
           });
-      }
-    });
+      },
 
-    hookApi.registerApi({
-      beforeDisposition: function (dispositionData) {
-        logMessage(`beforeDisposition ${JSON.stringify(dispositionData)}`);
+      // on object selected, update the CAV
+      objectSelected: (selectedObject) => {
+        logMessage(`Object Selected ${JSON.stringify(selectedObject)}`);
 
-        // get the record id from the component
-        var recordId = cmp.get("v.recordId");
-        logMessage(`Currently In-Focus Record Id: ${recordId}`);
-
-        // IMPORTANT - The CAV MUST be on the campaign profile layout
-        let updateCavList = [
-          { id: `${cavsDict["Salesforce"]["salesforce_id"]["id"]}`, value: recordId }
-        ]
-
-        logMessage(`Preparing CAV update with cavList: ${JSON.stringify(updateCavList)}`);
-
-        // update the CAVs on the call
-        interactionApi.setCav({
-          interactionId: dispositionData.interactionData.interactionId,
-          cavList: updateCavList
-        });
-
-        // obtain confirmation from the agent before proceeding
-        return Promise.resolve({
-          status: {
-            statusCode: Five9.CrmSdk.HookStatusCode.Confirmation,
-            message: `Do you weant to associate this call with ${recordId}?`,
-            messageHeader: 'Confirmation'
+        let selectedObjectRecordId = selectedObject.crmObject.id;
+        let urlParts = selectedObject.crmObject.metadata.url.split("/");
+        // for each part of the url, check if it is the record id
+        urlParts.forEach((part) => {
+          if (part.indexOf("recordId") > -1) {
+            selectedRecordId = urlParts[recordIdIndex];
+            logMessage(`Selected Record Id IS NOW: ${selectedObjectRecordId}`);
+            // IMPORTANT - The CAV MUST be on the campaign profile layout
+            updateSalesforceIdCAV(selectedRecordId);
           }
         });
-      }
+      },
+
+      // call accepted comes after call start event, leaving here in case a future action is needed
+      callAccepted: (acceptedCall) => {
+        logMessage(`Call Accepted ${JSON.stringify(acceptedCall)}`);
+      },
+    });
+
+    // register callbacks to the hook API
+    hookApi.registerApi({
+      // on screen pop, set the selectedRecordId
+      afterScreenPop: function (screenPopData) {
+        logMessage(`afterScreenPop ${JSON.stringify(screenPopData)}`);
+        // if only one screen pop object is returned, set the selected record id and update the CAV
+        if (screenPopData.screenPopObjects.length === 1) {
+          selectedRecordId = screenPopData.screenPopObjects[0].params.recordId;
+          updateSalesforceIdCAV(selectedRecordId);
+        }
+        return Promise.resolve({
+          status: {
+            statusCode: Five9.CrmSdk.HookStatusCode.Proceed,
+          },
+        });
+      },
+
+      // on disposition, check if the CAV is set to the correct record id
+      // NOTE - it is possible to still have the CAV set to the wrong record id 
+      //        if the agent disposition timer expires and the call is dispositioned
+      beforeDisposition: function (dispositionData) {
+        logMessage(`beforeDisposition ${JSON.stringify(dispositionData)}`);
+        // get the in-focus record id from the component
+        let inFocusRecordId = cmp.get("v.recordId");
+        logMessage(`Currently In-Focus Record Id: ${inFocusRecordId}`);
+        logMessage(`          Selected Record Id: ${selectedRecordId}`);
+
+        // update the CAVs on the call if mismatch between the current record id and the CAV
+        if (selectedRecordId != inFocusRecordId) {
+          // obtain confirmation from the agent before proceeding
+          logMessage(
+            `Selected Record Id: ${selectedRecordId} does not match ${inFocusRecordId}}`
+          );
+          return Promise.resolve({
+            status: {
+              statusCode: Five9.CrmSdk.HookStatusCode.Confirmation,
+              message: `The record selected doesn't match the one in focus (${inFocusRecordId}).  Do you want to proceed?`,
+              messageHeader: "Confirmation",
+            },
+          });
+        }
+        logMessage(`No CAV update required`);
+        return Promise.resolve({
+          status: {
+            statusCode: Five9.CrmSdk.HookStatusCode.Proceed,
+          },
+        });
+      },
     });
   },
 
@@ -70,5 +133,5 @@
     // Retrieve the record information using LDS
     var recordLoader = cmp.find("recordLoader");
     recordLoader.reloadRecord();
-  }
-})
+  },
+});
