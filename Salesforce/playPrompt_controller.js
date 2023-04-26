55d8602a7739377f636ce321ec9edaa1a88acdb4
Andrew Willey <andrew.willey@five9.com>
2023-04-26 09:02:33 -0500

    playprompt works

--- Salesforce/playPrompt_controller.js
+++ Salesforce/playPrompt_controller.js
@@ -1,89 +1,167 @@
 ({
   f9libLoaded: function (cmp, evt, helper) {
-    // This customization sets the Salesforce.salesforce_id CAV on the call
-    // when the agent selects a new related-to record in the adapter and
-    // also asks the agent to verify the disposition action if the record
-    // selected does not match the record in focus.
-
-    // IMPORTANT - The Salesforce.salesforce_id CAV MUST be on the
-    // campaign profile layout or this customization will not work.
+    // This customization plays a prompt on connect/Talking
+    // The prompt identified must be assigned to a skill that the agent is assigned to
 
     const hookApi = window.Five9.CrmSdk.hookApi();
     const interactionApi = window.Five9.CrmSdk.interactionApi();
 
-    let promptPlayed = false;
+    // set the name of the target prompt to play on connect/Talking
+    const autoPromptPlayName = "QRL";
+
+    let currentInteractionId = undefined;
 
     let availablePrompts = undefined
+    
+    // playPrompt function takes a promptId as input and plays the corresponding prompt
+    // If replayPrompt is true, the prompt will be played even if it has already been played during the interaction   
+    const playPrompt = (promptId, replayPrompt = false) => {
+      cons.log(`Playing Prompt ${promptId}`)
 
-    // initialize logging variables
+      // check localstorage to see if the prompt has already been played
+      //if (localStorage.getItem(`promptPlayed_${currentInteractionId}`) && !replayPrompt) {
+      let playedStatus = localStorage.getItem(`promptPlayed_${currentInteractionId}`)
+      if (!playedStatus) {
+        interactionApi.executeRestApi(
+          {
+            path: `/appsvcs/rs/svc/agents/${f9UserId}/interactions/calls/${currentInteractionId}/audio/player/play_prompt`,
+            method: 'PUT',
+            payload: JSON.stringify({
+              "value": promptId
+            })
+          }).then(function (result) {
+            // resolve handler
+            cons.log("Prompt Played Result:", result)
+            // add a localstorage variable to indicate that the prompt has been played
+            localStorage.setItem(`promptPlayed_${currentInteractionId}`, true);
+          }, function (result) {
+            // reject handler
+            cons.log("Prompt Played FAILED:", result)
+          });
+      } else {
+        cons.log("Prompt already played")
+      }
+    }
+
+    // playPromptByName function takes the name of the target prompt as input and retrieves the prompt ID
+    // Then, it calls playPrompt with the retrieved prompt ID
+    const playPromptByName = (targetPromptName) => {
+      cons.log("Getting available prompts")
+      interactionApi.executeRestApi(
+        {
+          path: `/appsvcs/rs/svc/agents/${f9UserId}/prompts`,
+          method: 'GET',
+          payload: null
+        }).then(function (result) {
+          // resolve handler
+          availablePrompts = JSON.parse(result.response);
+          cons.log("Available Prompts:", availablePrompts)
+          // result will look like this:
+          // [{"id":"222995","name":"QRL","description":"This call may be recorded for quality control and trianing purposes","duration":4141},{"id":"222975","name":"someskill_vm_greeting","description":"hi leave a message","duration":1496}]
+
+          // find the prompt with the target name by looping through each prompt in the result.response array
+          availablePrompts.forEach((prompt) => {
+            if (prompt.name === targetPromptName) {
+              cons.log(`Found prompt ${targetPromptName} with id ${prompt.id}`)
+              playPrompt(prompt.id)
+            }
+          }
+          )
+
+        }, function (result) {
+          // reject handler
+          cons.log("FAILED to get prompts:", result)
+        });
+    }
+
+    // Custom logging class to add a prefix and timestamp to the console log for easy identification
+    // initialize logging variables  
     const defaultLoggingPrefix = "[*****]";
-    const allowedLoggingModes = ["warn", "log", "info", "debug"];
-
-    let f9OrgId = undefined;
-    let f9UserId = undefined;
-
-    interactionApi
-        .getMetadata()
-        .then((metadataResponse) => {
-        console2.log(`"Metadata Response:\n${JSON.stringify(metadataResponse)}`)
-        f9OrgId = metadataResponse.tenantId;
-        f9UserId = metadataResponse.agentId;
-        console2.log(` OrgId: ${f9OrgId}`);
-        console2.log(`UserId: ${f9UserId}`);
-    })
-
+    const allowedLoggingModes = ["error", "warn", "log", "info", "debug"];
     class consoleLogger {
       constructor(prefix = defaultLoggingPrefix) {
-        this.prefix = prefix
+        this.prefix = prefix;
 
         // for each mode in allowedLoggingModes, create a function
         // that will log the message with the mode
         allowedLoggingModes.forEach((mode) => {
-          this[mode] = (message) => {
+          this[mode] = (...messages) => {
             const d = new Date();
             console[mode](
-              `${this.prefix} (${d.toLocaleString(
-                undefined, {
-                  year: "numeric",
-                  month: "2-digit",
-                  day: "2-digit",
-                  hour: "2-digit",
-                  minute: "2-digit",
-                  second: "2-digit",
-                  hour12: false, // use 24-hour time format
-                }
-              )}) ${message}`
+              `${this.prefix} (${d.toLocaleString(undefined, {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+                hour12: false, // use 24-hour time format
+              })}) ${messages
+                .map((message) => {
+                  if (typeof message === "object") {
+                    return `\n${JSON.stringify(message)}`;
+                  } else {
+                    return message;
+                  }
+                })
+                .join(" ")}`
             );
           };
         });
       }
     }
+    const cons = new consoleLogger();
 
-    let console2 = new consoleLogger();
+    let f9OrgId = undefined;
+    let f9UserId = undefined;
 
-
+    interactionApi
+      .getMetadata()
+      .then((metadataResponse) => {
+        cons.log(`"Metadata Response:\n${JSON.stringify(metadataResponse)}`)
+        f9OrgId = metadataResponse.tenantId;
+        f9UserId = metadataResponse.agentId;
+        cons.log(` OrgId: ${f9OrgId}`);
+        cons.log(`UserId: ${f9UserId}`);
+      })
 
 
     // register callbacks to the interaction API
     interactionApi.subscribe({
-
       // on call accepted, get the domain CAVs and map to a dictionary for easy lookup
-      callAccepted: (acceptedCall) => {
-        console2.log(`Call Started ${JSON.stringify(acceptedCall)}`);
-        // get the available prompts
+      callAccepted: (interactionSubscriptionEvent) => {
+        cons.log("Call Accepted:", interactionSubscriptionEvent);
 
       },
-
+      // When a call starts, set the current interaction ID and play the prompt with the specified name      
+      callStarted: (interactionSubscriptionEvent) => {
+        cons.log("Call Started:", interactionSubscriptionEvent);
+        currentInteractionId = interactionSubscriptionEvent.callData.interactionId;
+        cons.log(`Current Interaction Id: ${currentInteractionId}`)
+        // get the available prompts
+        interactionApi
+        .getMetadata()
+        .then((metadataResponse) => {
+          cons.log(`"Metadata Response:\n${JSON.stringify(metadataResponse)}`)
+          f9OrgId = metadataResponse.tenantId;
+          f9UserId = metadataResponse.agentId;
+          cons.log(` OrgId: ${f9OrgId}`);
+          cons.log(`UserId: ${f9UserId}`);
+          playPromptByName(autoPromptPlayName);
+        })
+      },
+      callEnded: (interactionSubscriptionEvent) => {
+        cons.log("Call Ended:", interactionSubscriptionEvent);
+      }
     });
 
     // subscribe to WsEvents
     interactionApi.subscribeWsEvent({
-        "4": function (payLoad, context) {
-            console2.log("Interaction Updated")
-            console2.log(`Context:\n${JSON.stringify(context)}`)
-            console2.log(` Params:\n${JSON.stringify(payLoad)}`)
-            
-        }
+      "4": function (payLoad, context) {
+        cons.debug("Interaction Updated")
+        cons.debug("context:", context)
+        cons.debug("context:", payLoad)
+      }
     });
   },
 
--- Salesforce/recordCavSync_controller.js
+++ Salesforce/recordCavSync_controller.js
@@ -12,13 +12,14 @@
     const hookApi = window.Five9.CrmSdk.hookApi();
     const interactionApi = window.Five9.CrmSdk.interactionApi();
 
+    // Custom logging class to add a prefix and timestamp to the console log for easy identification
     // initialize logging variables  
     const defaultLoggingPrefix = "[*****]";
     const allowedLoggingModes = ["error", "warn", "log", "info", "debug"];
     class consoleLogger {
       constructor(prefix = defaultLoggingPrefix) {
         this.prefix = prefix;
-    
+
         // for each mode in allowedLoggingModes, create a function
         // that will log the message with the mode
         allowedLoggingModes.forEach((mode) => {
@@ -47,9 +48,19 @@
         });
       }
     }
-    
 
-    let cons = new consoleLogger();
+    const cons = new consoleLogger();
+
+    class DomainCAVs {
+      constructor(cavList) {
+        // this.cavList = cavList;
+        cavList.forEach((cav) => {
+          this[cav.group] = this[cav.group] || {};
+          this[cav.group][cav.name] = cav;
+        });
+        cons.debug("CAV Definitions:", this);
+      }
+    }
 
     // initialize f9libLoaded scope variables
     // dictionary to hold the domain CAVs for easy lookup
@@ -67,7 +78,7 @@
           value: salesforceId,
         },
       ];
-      cons.log(`CAV update with cavList: ${JSON.stringify(updateCavList)}`);
+      cons.log("CAV update with cavList:", updateCavList);
       interactionApi.setCav(
         {
           interactionId: currentInteractionId,
@@ -81,31 +92,31 @@
 
       // on call accepted, get the domain CAVs and map to a dictionary for easy lookup
       callStarted: (startedCall) => {
-        cons.log(`Call Started ${JSON.stringify(startedCall)}`);
+        cons.log("Call Started", startedCall);
         currentInteractionId = startedCall.callData.interactionId;
         interactionApi
           .getCav({ interactionId: startedCall.callData.interactionId })
           .then((domainCavsReturned) => {
             cavDefinitions = new DomainCAVs(domainCavsReturned);
-            cons.log(`Interaction API got cavList: ${JSON.stringify(cavDefinitions)}`);
+            cons.log("Interaction API got cavList:", cavDefinitions);
           });
       },
 
       // on call accepted, get the domain CAVs and map to a dictionary for easy lookup
       callAccepted: (acceptedCall) => {
-        cons.log(`Call Started ${JSON.stringify(acceptedCall)}`);
+        cons.log(`Call Accepted ${JSON.stringify(acceptedCall)}`);
 
       },
 
       // on object selected, update the CAV
       objectSelected: (selectedObject) => {
-        cons.log(`Object Selected ${JSON.stringify(selectedObject)}`);
+        cons.log("Object Selected:", selectedObject);
 
         let selectedObjectRecordId = selectedObject.crmObject.id || undefined;
-        cons.log(`Selected Record Id: ${selectedObjectRecordId}`);
+        cons.log("Selected Record Id:", selectedObjectRecordId);
         if (selectedObjectRecordId != undefined) {
           let urlParts = selectedObject.crmObject.metadata.url.split("/");
-          cons.debug(`URL Parts: ${JSON.stringify(urlParts)}`);
+          // cons.debug("URL Parts:", urlParts);
           // for each part of the url, check if it is the record id
           urlParts.forEach((part) => {
             if (part.includes(selectedObjectRecordId)) {
@@ -124,7 +135,7 @@
 
       // call accepted comes after call start event, leaving here in case a future action is needed
       callAccepted: (acceptedCall) => {
-        cons.log(`Call Accepted ${JSON.stringify(acceptedCall)}`);
+        cons.log("Call Accepted:", acceptedCall);
       },
     });
 
@@ -132,7 +143,7 @@
     hookApi.registerApi({
       // on screen pop, set the selectedRecordId
       afterScreenPop: function (screenPopData) {
-        cons.log(`afterScreenPop ${JSON.stringify(screenPopData)}`);
+        cons.log("afterScreenPop:", screenPopData);
         // if only one screen pop object is returned, set the selected record id and update the CAV
         if (screenPopData.screenPopObjects.length === 1) {
           selectedRecordId = screenPopData.screenPopObjects[0].params.recordId;
@@ -149,7 +160,7 @@
       // NOTE - it is possible to still have the CAV set to the wrong record id
       //        if the agent disposition timer expires and the call is dispositioned
       beforeDisposition: function (dispositionData) {
-        cons.log(`beforeDisposition ${JSON.stringify(dispositionData)}`);
+        cons.log("beforeDisposition:", dispositionData);
         // get the in-focus record id from the component
         let inFocusRecordId = cmp.get("v.recordId");
         cons.log(`Currently In-Focus Record Id: ${inFocusRecordId}`);
